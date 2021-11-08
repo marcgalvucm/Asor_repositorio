4e662644a92edbd488267b8939338c072ce06d18
marcgalvucm <93117639+marcgalvucm@users.noreply.github.com>
2021-11-08 09:33:41 +0100

    Ejercicio 5

---
+++ Practica 2.2/ejercicio5.cc
@@ -0,0 +1,17 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+int main(){
+
+    int puntero = open("/home/usuarioso/Desktop/ficherosPrueba/fichero.txt", O_CREAT, 645);
+
+    if(puntero == -1){
+        perror("Se ha producido un error al crear el fichero");
+    }
+
+}
