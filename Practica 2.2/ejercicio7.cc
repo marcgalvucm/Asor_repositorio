193200a7797cc9f166b3503803113c4dd7503a22
marcgalvucm <93117639+marcgalvucm@users.noreply.github.com>
2021-11-08 09:47:06 +0100

    Ejercicio 7

---
+++ Practica 2.2/ejercicio7.cc
@@ -0,0 +1,19 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+int main(){
+
+	umask(027);
+
+    int puntero = open("/home/usuarioso/Desktop/ficherosPrueba/fichero2.txt", O_CREAT, 645);
+
+    if(puntero == -1){
+        perror("Se ha producido un error al crear el fichero");
+    }
+
+}
